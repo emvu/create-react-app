ad5eac3cc836cea01666caf5ee970900dbc3f59f
Michal Wychowaniec <michal.wychowaniec@gmail.com>
2016-12-02 09:32:18 +0100

    openBrowser disabled

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -279,7 +279,7 @@ function runDevServer(host, port, protocol) {
     console.log();
 
     if (isInteractive) {
-      openBrowser(protocol + '://' + host + ':' + port + '/');
+      // openBrowser(protocol + '://' + host + ':' + port + '/');
     }
   });
 }
